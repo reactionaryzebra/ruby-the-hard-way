0429ee95fe0042ed641c1e5be125d1845dd02a3a
reactionaryzebra <reactionaryzebra@gmail.com>
2020-07-11 10:01:17 -0700

    finish exercise 5

---
+++ ex5.rb
@@ -0,0 +1,13 @@
+name = 'Justin'
+age = 30
+height = 72 # inches
+weight = 160 #lbs
+eyes = 'Brown'
+teeth = 'White-ish'
+hair = 'Brown'
+
+puts "Let's talk about #{name}"
+puts "I am #{height} inches tall"
+puts "I weigh #{weight}"
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
